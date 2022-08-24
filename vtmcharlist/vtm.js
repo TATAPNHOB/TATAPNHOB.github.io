d30b1b2d4b94da0e9de5371ee1526d44d84cf90a
TATAPNHOB <dantatarinov@gmail.com>
2022-08-24 23:43:54 +0300

    values

--- vtmcharlist/vtm.js
+++ vtmcharlist/vtm.js
@@ -5,12 +5,14 @@ $("body").on("click", ".dot", function() {
 function toggleDot(dot) {
     if (dot.attr("state") === "1") {
         dot.attr("state", "0");
+        dot.parent().children("input").attr("value", (parseInt(dot.attr("n")) - 1).toString());
         for (i = dot.parent().attr("max") - 1; i > dot.attr("n"); i--) {
             dot.parent().children(".dot[n=" + i.toString() + "]").attr("state", "0");
         }
     }
     else {
         dot.attr("state", "1");
+        dot.parent().children("input").attr("value", (parseInt(dot.attr("n"))).toString());
         for (i = 0; i < dot.attr("n"); i++) {
             dot.parent().children(".dot[n=" + i.toString() + "]").attr("state", "1");
         }
